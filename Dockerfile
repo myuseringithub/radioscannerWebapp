e1e15c9ef864e91c05a5619b4ea44953b82a4161
myuseringithub <sfmissive@gmail.com>
2016-09-24 20:56:21 +0300

    update to allow dynamic url.

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ WORKDIR /srv/projects/app/nodeServer
 COPY ./nodeServer/ /srv/projects/app/nodeServer
 COPY ./root/ /srv/projects/app/root
 
-RUN apt-get -y update && apt-get -y upgrade
+# RUN apt-get -y update && apt-get -y upgrade
 ENTRYPOINT npm install --yes \
    && node /srv/projects/app/nodeServer/server.js
--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,4 +1,16 @@
 var express = require('express');
+var httpProxy = require('http-proxy');
+
 var app = express();
+var apiProxy = httpProxy.createProxyServer();
+
+app.use("/stream", function(req, res) {
+    console.log('redirecting to Server1');
+    apiProxy.web(req, res, {target: 'http://198.178.123.14:8424/'});
+});
+
 app.use('/', express.static(__dirname + '/../root/'));
-app.listen(80, function() { console.log('listening')});
+
+app.listen(80, function() {
+    console.log('listening')
+});
