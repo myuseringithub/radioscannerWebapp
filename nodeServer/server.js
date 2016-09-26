8593cef53b2c5ef5667459a39b551f2ae46931a5
myuseringithub <sfmissive@gmail.com>
2016-09-27 00:11:37 +0300

    Woking cross-origin, seperated nodejs from apache server.

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var httpProxy = require('http-proxy');
-
+var http = require('http');
+var request = require('request');
 var app = express();
-var apiProxy = httpProxy.createProxyServer();
-
-app.use("/stream", function(req, res) {
-    console.log('redirecting to Server1');
-    apiProxy.web(req, res, {target: 'http://198.178.123.14:8424/'});
-});
 
 app.use('/', express.static(__dirname + '/../root/'));
-
 app.listen(80, function() {
-    console.log('listening')
+    console.log('SZN - listening on port 80.')
 });
--- nodejsWeb.dockerfile
+++ nodejsWeb.dockerfile
@@ -6,6 +6,10 @@ WORKDIR /srv/projects/app/nodeServer
 COPY ./nodeServer/ /srv/projects/app/nodeServer
 COPY ./root/ /srv/projects/app/root
 
+RUN npm install --yes \
+     && npm ls \
+     && npm install -g nodemon \
+     && mv /srv/projects/app/nodeServer/node_modules /srv/projects/app/node_modules 
+
 # RUN apt-get -y update && apt-get -y upgrade
-ENTRYPOINT npm install --yes \
-   && node /srv/projects/app/nodeServer/server.js
+CMD node /srv/projects/app/nodeServer/server.js
