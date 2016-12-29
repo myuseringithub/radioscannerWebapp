a31def9649f5576e8c929e48dacbb5910b04e167
MyUserInGithub <sfmissive@gmail.com>
2016-12-29 22:49:44 +0200

    added comments

--- shellScripts/testAndBuild.sh
+++ shellScripts/testAndBuild.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # LF - Make sure to change format type to UNIX "LF" in editor so that the file would work.
 
+# Current directory is the "working diretory" set by docker-compose which is "/app".
+
 # Download dependencies & run tests.
-cd /app/tests
+cd ./tests
 npm install
 npm run test
 
 # build app for production.
-cd /app/build
+cd ../build
 npm install
 npm run build
